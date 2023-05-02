42be5c9f8d54584cdcbdc9708d8f4960ec6a8dfa
Fabiano G Lentini <Fabiano.G.Lentini@gmail.com>
2023-05-02 08:46:41 -0400

    second update attempt

---
+++ 8kiyu/Count_of_positive_sum_of_negatives.js
@@ -0,0 +1,26 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+//Pass after revision
+
+function countPositivesSumNegatives(input) {
+    if (input == null || input.length == 0){
+      return []
+    }
+   
+   let positive = 0
+   let negative = 0 
+   
+   for (i=0; i< input.length; i++){
+     if (input[i]>0){
+       ++positive
+     }else{
+       negative += input[i]
+     }
+   }
+   return [positive,negative]
+ }
+ 
