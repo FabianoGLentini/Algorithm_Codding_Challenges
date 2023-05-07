ed80302b58178f47605c8af75db2c9c7451a070c
Fabiano G Lentini <Fabiano.G.Lentini@gmail.com>
2023-05-07 09:48:23 -0400

    code wars of the day

---
+++ 8kiyu/Remove_Exclamation_Marks.js
@@ -0,0 +1,8 @@
+// Remove exclamation marks
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+//firt pass
+
+function removeExclamationMarks(s) {
+    return s.replaceAll('!', '')
+  }
