1fae610e821b6a762c7b22ecdbeaa39a4165cf64
FabianoGLentini <fabiano.g.lentini@gmail.com>
2023-06-21 09:15:55 -0400

    Add: code wars solution -- lvl 8 kyu -- name: Grasshopper - Grade book

---
+++ 8kyu/Grasshopper - Grade book.js
@@ -0,0 +1,47 @@
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+//First Pass
+
+function getGrade (s1, s2, s3) {
+    //average var
+      // find the average of the 3 score 
+    const averageScore = (s1+s2+s3)/3
+    
+    //if statement to check value of score and return appropriate string start by lowest possible number and go up
+    if(averageScore < 60){
+      return "F"
+      
+    }else if(averageScore < 70){
+      return "D"
+      
+    }else if(averageScore < 80){
+      return "C"
+      
+    }else if(averageScore < 90){
+      return "B"
+      
+    }else{
+      return "A"
+    }
+    
+  }
+
+//Suggested Refractor code wars
+
+function getGrade (s1, s2, s3) {
+    avg = (s1+s2+s3)/3;
+    if (avg < 60)  return "F";
+      else if (avg < 70) return "D";
+      else if (avg < 80) return "C";
+      else if (avg < 90) return "B";
+      else return "A";
+  }
