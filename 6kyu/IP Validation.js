5837a455103e87417c1ae4ee95d8f391549950e6
FabianoGLentini <132173298+FabianoGLentini@users.noreply.github.com>
2023-11-21 09:50:41 -0500

    Update: code wars solution -- lvl 6 kyu -- Name: IP Validation

--- 6kyu/IP Validation.js
+++ 6kyu/IP Validation.js
@@ -40,9 +40,12 @@ function isValidIP(str) {
 
 function isValidIP(str) {
     const octets = str.split('.')							// Split into octets
+
+
     return (octets.length === 4) &&						// Ensure str has exactly 4 octets
         octets.reduce((acc, octet) =>						// Check all octets
-          (acc === true) &&												// Ensure prior octets okay
+         
+        (acc === true) &&												// Ensure prior octets okay
            (String(Number(octet)) === octet) &&		// Ensure octet has nothing funky like spaces, leading 0's, ...
             (Number(octet) >= 0) &&									// Ensure octet is 0 or more
              (Number(octet) <= 255)									// Ensure octet is 255 or less
