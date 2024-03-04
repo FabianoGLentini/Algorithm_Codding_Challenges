7505edff139622094762a2c726438784e76a89cd
FabianoGLentini <fabiano.g.lentini@gmail.com>
2024-03-04 07:21:06 -0500

    Add: code wars solution -- lvl 7 kyu -- Name: Number of Decimal Digits

---
+++ 7kyu/Number of Decimal Digits.js
@@ -0,0 +1,16 @@
+// Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+// All inputs will be valid.
+
+//My First Pass
+
+function digits(n) {
+    return String(n).split("").length
+  }
+
+
+//Code Wars Refractor
+
+function digits(n) {
+    return n.toString().length;
+  }
