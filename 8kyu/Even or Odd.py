99621ad1fcaf91f34157b374bbe1551fdffe7f04
FabianoGLentini <fabiano.g.lentini@gmail.com>
2024-07-27 10:17:38 -0400

    Add: (Python) code wars solution -- lvl 8 kyu -- Name: Even or Odd

---
+++ 8kyu/Even or Odd.py
@@ -0,0 +1,16 @@
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+# My Python Solution
+
+def even_or_odd(number):
+   if number % 2 == 0 :
+    return "Even"
+
+   else:
+    return "Odd" 
+
+# Code Wars Alternatives
+
+def even_or_odd(number):
+	return 'Odd' if number % 2 else 'Even'
+
