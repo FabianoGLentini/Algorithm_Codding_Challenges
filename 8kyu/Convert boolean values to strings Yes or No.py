df88aa77738dce7508821b52bc9b627a16ce6f43
FabianoGLentini <fabiano.g.lentini@gmail.com>
2024-08-12 07:20:27 -0400

    Add: (Python) code wars solution -- lvl 8 kyu -- Name: Convert boolean values to strings 'Yes' or 'No'.

---
+++ 8kyu/Convert boolean values to strings Yes or No.py
@@ -0,0 +1,13 @@
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+# My Python Solution 
+
+def bool_to_word(boolean):
+  return "Yes" if boolean == True else "No"
+
+
+# Code Wars Alternative
+
+def bool_to_word(bool):
+    return "Yes" if bool else "No"
