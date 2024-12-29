ec456a9026af283cfd85f35d5f3463e1c77eea34
Fabiano Lentini <fabiano.g.lentini@gmail.com>
2024-12-28 20:21:55 -0500

    Add: (Python) code wars solution -- lvl 6 kyu -- Name: Are they the same?

---
+++ 6kyu/Are they the same.py
@@ -0,0 +1,61 @@
+"""
+Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+Invalid arrays
+If, for example, we change the first number to something else, comp is not returning true anymore:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+Remarks
+a or b might be [] or {} (all languages except R, Shell).
+a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+"""
+
+# My Python Solution
+def comp(array1, array2):
+    if array1 == None or array2 == None:
+        return False
+    
+    if len(array1) != len(array2):
+        return False
+    
+    for el in array1:
+        if (el*el) in array2:
+            array2.remove(el*el)
+            
+        else:
+            return False
+        
+    return True 
+
+# Code Wars Alternative
+def comp(array1, array2):
+    try:
+        return sorted([i ** 2 for i in array1]) == sorted(array2)
+    except:
+        return False
+    
+# Chat Gbt-4 Recommendations
+from collections import Counter
+
+def comp(array1, array2):
+    if array1 is None or array2 is None:
+        return False
+    
+    # Check if the square counts match
+    return Counter(array2) == Counter(x * x for x in array1)
